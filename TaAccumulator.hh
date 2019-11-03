75d1c977ff38a5dd5f055dc0d6625e9c0e7e8889
adaq <apar@adaq1.jlab.org>
2019-11-03 18:05:03 -0500

    backup

---
+++ TaAccumulator.hh
@@ -0,0 +1,26 @@
+#ifndef __TaAccumulator_hh__
+#define __TaAccumulator_hh__
+#include "Rtypes.h"
+
+class TaAccumulator{
+public:
+  TaAccumulator();
+  virtual ~TaAccumulator();
+  void Update(Double_t s, Double_t t);
+  void Update(Double_t ss);
+  void Merge( TaAccumulator);
+  void Zero();
+  inline Double_t GetM2() const {return M2;};
+  inline Double_t GetMean1() const {return mu1;};
+  inline Double_t GetMean2() const {return mu2;};
+  inline Double_t GetN() const {return n;};
+private:
+  Double_t M2;
+  Double_t mu1;
+  Double_t mu2;
+  Double_t n;
+
+  ClassDef(TaAccumulator,0);
+};
+typedef std::vector<TaAccumulator> AccumulatorArray;
+#endif
