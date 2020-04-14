4e48177f9c91966f1cdfb07665c056cef3f27257
leafybillow <tao.ye@stonybrook.edu>
2020-04-13 22:56:41 -0400

    recent update

--- find_beamoff_run.cc
+++ find_beamoff_run.cc
@@ -1,47 +1,7 @@
 #include "src/TaAccumulator.cc"
 #include "src/TaEventRing.cc"
 #include "LoadNormalizationMap.C"
-
-TString generate_cut(vector<Double_t> fEventCounterArray){
-  TString fret;
-  Int_t length = fEventCounterArray.size();
-  if(length==0)
-    return "1==0";
-  
-  Int_t start_pt= fEventCounterArray[0];
-  Int_t end_pt = -1;
-  vector<pair<Int_t,Int_t> > fRangeArray;
-  for(int i=1;i<length;i++){
-    if(fEventCounterArray[i]-fEventCounterArray[i-1]>1){
-      end_pt = fEventCounterArray[i-1];
-      fRangeArray.push_back(make_pair(start_pt,end_pt));
-      start_pt = fEventCounterArray[i];
-    }
-
-    if(i==length-1){
-      end_pt = fEventCounterArray[i];
-      fRangeArray.push_back(make_pair(start_pt,end_pt));
-    }
-  }
-  auto iter = fRangeArray.begin();
-  while(iter!=fRangeArray.end()){
-    if( (*iter).first == (*iter).second){
-      iter++;
-      continue;
-    }
-    if(fret=="")
-      fret += Form("(pattern_number>=%d && pattern_number<=%d )",
-		   (*iter).first, (*iter).second);
-    else
-      fret += Form("|| (pattern_number>=%d && pattern_number<=%d )",
-		   (*iter).first, (*iter).second);
-    
-    iter++;
-  }
-  
-  return fret;
-}
-
+#include "utilities.cc"
 void find_beamoff_run(Int_t run_number){
   ROOT::EnableImplicitMT(2);
   gStyle->SetStatH(0.35);
@@ -140,8 +100,10 @@ void find_beamoff_run(Int_t run_number){
       }else if(ch_name.Contains("diff")){
 	TString alias_name = ch_name;
 	TString raw_name = ch_name.ReplaceAll("diff_","");
-	if(mul_tree->GetBranch("yield_"+raw_name)==NULL)
+	if(mul_tree->GetBranch("yield_"+raw_name)==NULL){
 	  device_list.erase(iter_dev);
+	  continue;
+	}
 	mul_tree->SetAlias(alias_name,Form("asym_%s * yield_%s",raw_name.Data(),raw_name.Data()));
 	iter_dev++;
       }else{
