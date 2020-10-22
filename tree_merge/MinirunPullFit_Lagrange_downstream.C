e774c2cc234b7a977329ef86e8cef8b50af11d2d
Tao Ye <tao.ye@stonybrook.edu>
2020-10-22 15:37:05 -0400

    mini-run pull distribution macros

--- tree_merge/MinirunPullFit_Lagrange_downstream.C
+++ tree_merge/MinirunPullFit_Lagrange_downstream.C
@@ -1,4 +1,4 @@
-void MinirunPullFit_Lagrange(){
+void MinirunPullFit_Lagrange_downstream(){
   TFile *input = TFile::Open("./rootfiles/MergedLagrange_all_slugs.root");
   TTree *mini_tree = (TTree*)input->Get("mini_lagrall");
   mini_tree->AddFriend("mini_info","./rootfiles/runinfo_all_slugs.root");
@@ -6,32 +6,32 @@ void MinirunPullFit_Lagrange(){
   TCanvas *c1 = new TCanvas("c1","c1",1200,500);
   TCanvas *c2 = new TCanvas("c2","c2",600,600);
 
-  mini_tree->Draw("spin*lagr_asym_us_avg*1e9: lagr_asym_us_avg.err*1e9:run+mini/10.0",
+  mini_tree->Draw("spin*lagr_asym_ds_avg*1e9: lagr_asym_ds_avg.err*1e9:run+mini/10.0",
 		  "arm_flag==0 && kGood==1","goff");
   TGraphErrors* mger_both = new TGraphErrors(mini_tree->GetSelectedRows(),
 					     mini_tree->GetV3(),mini_tree->GetV1(),
 					     0,mini_tree->GetV2());
   mger_both->SetMarkerStyle(7);
-  mini_tree->Draw("spin*lagr_asym_usr*1e9: lagr_asym_usr.err*1e9:run+mini/10.0",
+  mini_tree->Draw("spin*lagr_asym_dsr*1e9: lagr_asym_dsr.err*1e9:run+mini/10.0",
 		  "arm_flag==1 && kGood==1","goff");
-  TGraphErrors* mger_usr = new TGraphErrors(mini_tree->GetSelectedRows(),
+  TGraphErrors* mger_dsr = new TGraphErrors(mini_tree->GetSelectedRows(),
 					     mini_tree->GetV3(),mini_tree->GetV1(),
 					     0,mini_tree->GetV2());
-  mger_usr->SetMarkerStyle(7);
+  mger_dsr->SetMarkerStyle(7);
 
-  mini_tree->Draw("spin*lagr_asym_usl*1e9: lagr_asym_usl.err*1e9:run+mini/10.0",
+  mini_tree->Draw("spin*lagr_asym_dsl*1e9: lagr_asym_dsl.err*1e9:run+mini/10.0",
 		  "arm_flag==2 && kGood==1","goff");
-  TGraphErrors* mger_usl = new TGraphErrors(mini_tree->GetSelectedRows(),
+  TGraphErrors* mger_dsl = new TGraphErrors(mini_tree->GetSelectedRows(),
 					    mini_tree->GetV3(),mini_tree->GetV1(),
 					    0,mini_tree->GetV2());
-  mger_usl->SetMarkerStyle(7);
+  mger_dsl->SetMarkerStyle(7);
 
   TMultiGraph *mgall = new TMultiGraph();
   mgall->Add(mger_both,"p");
-  mgall->Add(mger_usr,"p");
-  mgall->Add(mger_usl,"p");
+  mgall->Add(mger_dsr,"p");
+  mgall->Add(mger_dsl,"p");
   
-  mgall->SetTitle("PREX Lagrange Corrected Asymmetry(ppb) ; run; (ppb)"); 
+  mgall->SetTitle("PREX Lagrange Corrected Asymmetry(ppb) - Downstream Main; run; (ppb)"); 
   gStyle->SetOptFit(1);
   mgall->Draw("AP");
   mgall->Fit("pol0");
@@ -44,11 +44,11 @@ void MinirunPullFit_Lagrange(){
   TH1D* hpull = new TH1D("hpull","Pull Fit", 150,-6,6);
   // gStyle->SetOptStat(0);
   // gStyle->SetOptFit(0);
-  mini_tree->Draw(Form( "(spin*lagr_asym_us_avg*1e9 - %f)/(lagr_asym_us_avg.err*1e9)>>hpull",mean_fit),
+  mini_tree->Draw(Form( "(spin*lagr_asym_ds_avg*1e9 - %f)/(lagr_asym_ds_avg.err*1e9)>>hpull",mean_fit),
 		  "arm_flag==0 && kGood==1","goff");
-  mini_tree->Draw(Form( "(spin*lagr_asym_usr*1e9 - %f)/(lagr_asym_usr.err*1e9) >>+hpull",mean_fit),
+  mini_tree->Draw(Form( "(spin*lagr_asym_dsr*1e9 - %f)/(lagr_asym_dsr.err*1e9) >>+hpull",mean_fit),
 		  "arm_flag==1 && kGood==1","goff");
-  mini_tree->Draw(Form( "(spin*lagr_asym_usl*1e9 - %f)/(lagr_asym_usl.err*1e9) >>+hpull",mean_fit),
+  mini_tree->Draw(Form( "(spin*lagr_asym_dsl*1e9 - %f)/(lagr_asym_dsl.err*1e9) >>+hpull",mean_fit),
 		  "arm_flag==2 && kGood==1","goff");
   hpull->Draw("PE");
   hpull->SetMarkerStyle(7);
@@ -62,6 +62,6 @@ void MinirunPullFit_Lagrange(){
   hpull->GetYaxis()->SetTitleFont(22);
   hpull->GetYaxis()->SetLabelFont(22);
 
-  c1->SaveAs("prex_lagrange_minirun_pullfit.pdf");
-  c2->SaveAs("prex_lagrange_minirun_pull_distribution.pdf");
+  c1->SaveAs("prex_lagrange_downstream_minirun_pullfit.pdf");
+  c2->SaveAs("prex_lagrange_downstream_minirun_pull_distribution.pdf");
 }
--- tree_merge/MinirunPullFit_dither.C
+++ tree_merge/MinirunPullFit_dither.C
@@ -1,25 +1,26 @@
-void MinirunPullFit_Lagrange(){
-  TFile *input = TFile::Open("./rootfiles/MergedLagrange_all_slugs.root");
-  TTree *mini_tree = (TTree*)input->Get("mini_lagrall");
+void MinirunPullFit_dither(){
+  TFile *input = TFile::Open("./rootfiles/MergedSum_all_slugs.root");
+  TTree *mini_tree = (TTree*)input->Get("burst_mulc_dit");
+  mini_tree->AddFriend("burst_mulc_dit_combo");
   mini_tree->AddFriend("mini_info","./rootfiles/runinfo_all_slugs.root");
   
   TCanvas *c1 = new TCanvas("c1","c1",1200,500);
   TCanvas *c2 = new TCanvas("c2","c2",600,600);
 
-  mini_tree->Draw("spin*lagr_asym_us_avg*1e9: lagr_asym_us_avg.err*1e9:run+mini/10.0",
+  mini_tree->Draw("spin*dit_asym_us_avg*1e9: dit_asym_us_avg.err*1e9:run+mini/10.0",
 		  "arm_flag==0 && kGood==1","goff");
   TGraphErrors* mger_both = new TGraphErrors(mini_tree->GetSelectedRows(),
 					     mini_tree->GetV3(),mini_tree->GetV1(),
 					     0,mini_tree->GetV2());
   mger_both->SetMarkerStyle(7);
-  mini_tree->Draw("spin*lagr_asym_usr*1e9: lagr_asym_usr.err*1e9:run+mini/10.0",
+  mini_tree->Draw("spin*dit_asym_usr*1e9: dit_asym_usr.err*1e9:run+mini/10.0",
 		  "arm_flag==1 && kGood==1","goff");
   TGraphErrors* mger_usr = new TGraphErrors(mini_tree->GetSelectedRows(),
 					     mini_tree->GetV3(),mini_tree->GetV1(),
 					     0,mini_tree->GetV2());
   mger_usr->SetMarkerStyle(7);
 
-  mini_tree->Draw("spin*lagr_asym_usl*1e9: lagr_asym_usl.err*1e9:run+mini/10.0",
+  mini_tree->Draw("spin*dit_asym_usl*1e9: dit_asym_usl.err*1e9:run+mini/10.0",
 		  "arm_flag==2 && kGood==1","goff");
   TGraphErrors* mger_usl = new TGraphErrors(mini_tree->GetSelectedRows(),
 					    mini_tree->GetV3(),mini_tree->GetV1(),
@@ -31,7 +32,7 @@ void MinirunPullFit_Lagrange(){
   mgall->Add(mger_usr,"p");
   mgall->Add(mger_usl,"p");
   
-  mgall->SetTitle("PREX Lagrange Corrected Asymmetry(ppb) ; run; (ppb)"); 
+  mgall->SetTitle("PREX Dithering Corrected Asymmetry(ppb) ; run; (ppb)"); 
   gStyle->SetOptFit(1);
   mgall->Draw("AP");
   mgall->Fit("pol0");
@@ -44,11 +45,11 @@ void MinirunPullFit_Lagrange(){
   TH1D* hpull = new TH1D("hpull","Pull Fit", 150,-6,6);
   // gStyle->SetOptStat(0);
   // gStyle->SetOptFit(0);
-  mini_tree->Draw(Form( "(spin*lagr_asym_us_avg*1e9 - %f)/(lagr_asym_us_avg.err*1e9)>>hpull",mean_fit),
+  mini_tree->Draw(Form( "(spin*dit_asym_us_avg*1e9 - %f)/(dit_asym_us_avg.err*1e9)>>hpull",mean_fit),
 		  "arm_flag==0 && kGood==1","goff");
-  mini_tree->Draw(Form( "(spin*lagr_asym_usr*1e9 - %f)/(lagr_asym_usr.err*1e9) >>+hpull",mean_fit),
+  mini_tree->Draw(Form( "(spin*dit_asym_usr*1e9 - %f)/(dit_asym_usr.err*1e9) >>+hpull",mean_fit),
 		  "arm_flag==1 && kGood==1","goff");
-  mini_tree->Draw(Form( "(spin*lagr_asym_usl*1e9 - %f)/(lagr_asym_usl.err*1e9) >>+hpull",mean_fit),
+  mini_tree->Draw(Form( "(spin*dit_asym_usl*1e9 - %f)/(dit_asym_usl.err*1e9) >>+hpull",mean_fit),
 		  "arm_flag==2 && kGood==1","goff");
   hpull->Draw("PE");
   hpull->SetMarkerStyle(7);
@@ -62,6 +63,6 @@ void MinirunPullFit_Lagrange(){
   hpull->GetYaxis()->SetTitleFont(22);
   hpull->GetYaxis()->SetLabelFont(22);
 
-  c1->SaveAs("prex_lagrange_minirun_pullfit.pdf");
-  c2->SaveAs("prex_lagrange_minirun_pull_distribution.pdf");
+  c1->SaveAs("prex_dither_minirun_pullfit.pdf");
+  c2->SaveAs("prex_dither_minirun_pull_distribution.pdf");
 }
